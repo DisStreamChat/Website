b60f6b22e100c61c22496e984f2b7e484c7c135a
David Snyder <davidgraygs4@gmail.com>
2020-05-15 21:18:54 -0400

    worked on header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, {useEffect, useState} from 'react';
 import firebase from "./firebase"
 import './App.css';
+import Header from './components/Header';
+import {HashRouter as Router} from "react-router-dom"
 
 function App() {
   return (
-    <main className="App light">
+    <main className="App">
+        <Router>
+            <Header/>
 
+        </Router>
     </main>
   );
 }
