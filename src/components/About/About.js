db5e824797609b21f850ad83133cb191d7b7bd33
David Snyder <davidgraygs4@gmail.com>
2020-05-17 12:28:51 -0400

    added tutorial component and changed file structure

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import "./About.css"
+import Tutorials from "./Tutorials"
 
 const About = () => {
     return (
         <main className="about-page">
             About
+            <Tutorials/>
         </main>
     );
 }
---
+++ src/components/About/Tutorials.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tutorials = () => {
+    return (
+        <div className="tutorials">
+            Tutorials
+        </div>
+    );
+}
+
+export default Tutorials;
