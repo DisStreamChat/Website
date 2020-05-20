ea83115dd231da8558d252b51ac59d0e422bfa01
David Snyder <davidgraygs4@gmail.com>
2020-05-20 17:14:31 -0400

    refactored team page and made it responsive

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -4,26 +4,34 @@ import "./Team.css"
 import GitHubIcon from '@material-ui/icons/GitHub';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import A from "../Shared/A"
+import Member from './Member';
 
 const Team = () => {
     return (
         <main className="main team-page">
             <h1 className="team-header">DisTwitchChat's Team</h1>
             <div className="members">
-                <A href="https://github.com/GypsyDangerous" newTab><img className="team-picture" src={`${process.env.PUBLIC_URL}/david.png`} width="320" alt="" /></A>
-                <A href="https://saintplaysthings.com" newTab><img className="team-picture" src={`${process.env.PUBLIC_URL}/kobe.png`} width="320" alt="" /></A>
-                <h1 className="team-title">David</h1>
-                <h1 className="team-title">Kobe</h1>
-                <h3 className="team-sub-title">Project Lead</h3>
-                <h3 className="team-sub-title">Software Engineer</h3>
-                <div className="socials">
-                    <A href="https://github.com/GypsyDangerous" newTab><GitHubIcon/></A>
-                    <A href="https://twitter.com/Snyderling_" newTab><TwitterIcon/></A>
-                </div>
-                <div className="socials">
-                    <A href="https://github.com/KobeLiesenborgs" newTab><GitHubIcon/></A>
-                    <A href="https://www.twitch.tv/saintplaysthings/" newTab><img src={`${process.env.PUBLIC_URL}/social-media.svg`} alt="" width="24"></img></A>
-                </div>
+                <Member
+                    imgUrl="https://github.com/GypsyDangerous"
+                    img={`${process.env.PUBLIC_URL}/david.png`}
+                    name="David"
+                    title="Founder"
+                    socials={[
+                        {link: "https://github.com/GypsyDangerous", icon: <GitHubIcon />},
+                        { link: "https://twitter.com/Snyderling_", icon: <TwitterIcon />}
+                        
+                    ]}
+                ></Member>
+                <Member
+                    imgUrl="https://saintplaysthings.com"
+                    img={`${process.env.PUBLIC_URL}/kobe.png`}
+                    name="Kobe"
+                    title="Co-Founder"
+                    socials={[
+                        { link: "https://github.com/KobeLiesenborgs", icon: <GitHubIcon /> },
+                        { link: "https://www.twitch.tv/saintplaysthings/", icon: <img src={`${process.env.PUBLIC_URL}/social-media.svg`} alt="" width="24"></img> }
+                    ]}
+                ></Member>
             </div>
         </main>
     );
