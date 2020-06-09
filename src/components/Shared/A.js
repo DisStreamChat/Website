3786fb55bc4f085d23476685e6aebcba9d26ac96
David Snyder <davidgraygs4@gmail.com>
2020-06-09 14:11:25 -0400

    updated custom a tag

--- src/components/Shared/A.js
+++ src/components/Shared/A.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import {Link} from "react-router-dom"
 
+const Anchor = ({children, ...rest}) => {
+    return <a {...rest}>{children}</a>
+}
 
 const A = props => {
+
+    const elementProps = {
+        href: props.href,
+        to: props.href,
+        className: props.className,
+        target: props.newTab && "_blank",
+        rel: props.newTab && "noopener noreferrer"
+    }
+
+    const Element = props.local ? Link : Anchor
+
     return (
-        <a href={props.href} className={props.className} target={props.newTab && "_blank"} rel={props.newTab && "noopener noreferrer"}>{props.children}</a>
+        <Element {...elementProps}>{props.children}</Element>
     );
 }
 
