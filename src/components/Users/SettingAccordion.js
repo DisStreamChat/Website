6edf8e2feb0588af3cdd6468e48b6165277f2c2b
David Snyder <davidgraygs4@gmail.com>
2020-06-11 16:21:15 -0400

    add accordion menu to settings

---
+++ src/components/Users/SettingAccordion.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useCallback, useState } from "react";
+
+const SettingAccordion = props => {
+    const [openItem, setOpenItem] = useState()
+
+    const clickHandler = useCallback(key => {
+        if(key === openItem){
+            setOpenItem("")
+        }else{
+            setOpenItem(key)
+        }
+    }, [openItem])
+
+    return (
+        <div>
+            {props.children.map(Element => (
+                React.cloneElement(Element, {onClick: clickHandler, open: Element.props.name === openItem})
+            ))}
+        </div>
+    );
+}
+
+export default SettingAccordion;
