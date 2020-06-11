798814012566cc5334168c60d9d3c5c470292fd9
David Snyder <davidgraygs4@gmail.com>
2020-06-11 15:42:18 -0400

    added eslint and pretter config

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true,
+            "modules": true
+        }
+    },
+    "rules": {
+        "quotes": [2, "double", { "avoidEscape": true }]
+    }
+}
