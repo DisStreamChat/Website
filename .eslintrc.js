9e8312d549200bbf7bae5c01aa0e056e8755b520
David Snyder <davidgraygs4@gmail.com>
2020-06-11 16:15:53 -0400

    updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,21 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true,
-            "modules": true
-        }
-    },
-    "rules": {
-        "quotes": [2, "double", { "avoidEscape": true }]
-    }
-}
+	extends: [
+		"plugin:react-hooks/recommended",
+		"eslint:recommended",
+		"plugin:react/recommended",
+	],
+	plugins: [
+		// ...
+		"react-hooks",
+	],
+	// parser: "babel-eslint",
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+			modules: true,
+		},
+	},
+	rules: {
+		quotes: [2, "double", { avoidEscape: true }],
+	},
+};
