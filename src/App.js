26a8dec932a6387ff8ba48a948242dd43736e8f6
David Snyder <davidgraygs4@gmail.com>
2020-06-28 19:40:18 -0400

    added banner notification

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ import Loader from "react-loader";
 
 import "./App.scss";
 import { AppContext } from "./contexts/Appcontext";
+import Banner from "./components/Shared/Banner";
+import { Button } from "@material-ui/core";
+import A from "./components/Shared/A"
 
 function App(props) {
 	const [userId, setUserId] = useState("");
@@ -114,6 +117,9 @@ function App(props) {
 					</div>{" "}
 					: <></>
 				</Switch>
+                <Banner message="DisStreamChat is in very early beta, you can help test it if you are a streamer">
+                <A newTab href="https://api.disstreamchat.com/discord"><Button className="banner-button">Learn More</Button></A>
+                </Banner>
 			</AppContext.Provider>
 		</Router>
 	) : (
