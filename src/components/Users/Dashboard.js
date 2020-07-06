2f537cece36dcbdbd1dec0eb3b27eb5efc5612b7
David Snyder <davidgraygs4@gmail.com>
2020-07-06 14:19:48 -0400

    went back to regular settings

--- src/components/Users/Dashboard.js
+++ src/components/Users/Dashboard.js
@@ -21,7 +21,7 @@ const Dashboard = props => {
 
 	useEffect(() => {
 		(async () => {
-			const settingsRef = await firebase.db.collection("defaults").doc("testSettings").get();
+			const settingsRef = await firebase.db.collection("defaults").doc("settings").get();
 			const settingsData = settingsRef.data().settings;
 			setDefaultSettings(settingsData);
 		})();
