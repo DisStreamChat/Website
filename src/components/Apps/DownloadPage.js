4ad9120981dd9ee1c4348a9e87f53e49316f4474
David Snyder <davidgraygs4@gmail.com>
2020-07-11 12:53:38 -0400

    added downlaod page for app

---
+++ src/components/Apps/DownloadPage.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import A from "../Shared/A";
+import { Button } from "@material-ui/core";
+import ArrowRightIcon from "@material-ui/icons/ArrowRight";
+
+const DownloadPage = () => {
+	return (
+		<div className="download-page">
+			<div className="left">
+				<h1>DisStreamChat Chat Client</h1>
+                <h2>All your stream chats in one place</h2>
+                <p>Keeping track of your stream can be really difficult, especially if you are streaming cross platform and have large discord community. DisStreamChat allows you have all your chats in one place so you can easily view and moderate the chat.</p>
+			</div>
+			<div className="right">
+				<div className="heading">
+					<h1>Download for Windows</h1>
+				</div>
+				<div className="download-links">
+					<A href="https://api.disstreamchat.com/app" newTab>
+						<Button className="download-button">Latest Version</Button>
+					</A>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default DownloadPage;
