f250b463493d12d6a5b3ec9fd823dd12e2e6a560
David Snyder <davidgraygs4@gmail.com>
2020-08-14 17:14:32 -0400

    remove github from community page

--- src/components/Community/Community.js
+++ src/components/Community/Community.js
@@ -1,24 +1,21 @@
-import React from 'react';
-
-import "./Community.scss"
-import GitHubIcon from '@material-ui/icons/GitHub';
+import React from "react";
+import "./Community.scss";
 
 const Community = () => {
-    return (
-        <>
-            <div className="header-area">
-                <h1 className="body-header">Join The Community!</h1>
-                <h3 className="body-subheader">Come Say Hi in the discord or contribute to the github!</h3>
-            </div>
-            <div className="community buttons">
-                <a href="https://api.disstreamchat.com/discord" target="_blank" rel="noreferrer noopener" className="discord-button">
-                    <img src={`${process.env.PUBLIC_URL}/discord.png`} alt="custom discord logo"></img>
-                    Join The Discord
-                </a>
-                <a href="https://github.com/DisStreamChat" target="_blank" rel="noreferrer noopener" className="github-button"><GitHubIcon /><span>Github</span></a>
-            </div>
-        </>
-    );
-}
+	return (
+		<>
+			<div className="header-area">
+				<h1 className="body-header">Join The Community!</h1>
+				<h3 className="body-subheader">Come Say Hi in the discord!</h3>
+			</div>
+			<div className="community buttons">
+				<a href="https://api.disstreamchat.com/discord" target="_blank" rel="noreferrer noopener" className="discord-button">
+					<img src={`${process.env.PUBLIC_URL}/discord.png`} alt="custom discord logo"></img>
+					Join The Discord
+				</a>
+			</div>
+		</>
+	);
+};
 
 export default Community;
