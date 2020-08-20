21186bde8611b43ec9a2c4d405655f42f63563b0
David Snyder <45735850+GypsyDangerous@users.noreply.github.com>
2020-08-20 11:14:19 -0400

    update settings

--- src/components/Users/Dashboard.js
+++ src/components/Users/Dashboard.js
@@ -24,7 +24,7 @@ const Dashboard = props => {
 
 	useEffect(() => {
 		(async () => {
-			const settingsRef = await firebase.db.collection("defaults").doc("settings13").get();
+			const settingsRef = await firebase.db.collection("defaults").doc("settings15").get();
 			const settingsData = settingsRef.data().settings;
 			setDefaultSettings(settingsData);
 		})();
