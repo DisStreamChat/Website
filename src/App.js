01d799afcd8af5b653cbe4a23b2f2b41e440f71c
David Snyder <davidgraygs4@gmail.com>
2020-08-22 12:31:05 -0400

    added very basic leaderboard

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ import Banner from "./components/Shared/Banner";
 import { Button } from "@material-ui/core";
 import A from "./components/Shared/A";
 import useSnapshot from "./hooks/useSnapshot";
+import LeaderBoard from "./components/LeaderBoard/LeaderBoard";
 
 function App(props) {
 	const [userId, setUserId] = useState("");
@@ -129,6 +130,7 @@ function App(props) {
 								<Route path="/privacy" component={PrivacyPolicy} />
 								<Route path="/terms" component={Terms} />
 								<Route path="/apps/download" component={DownloadPage} />
+                                <Route path="/leaderboard/:id" component={LeaderBoard}/>
 								<ProtectedRoute path="/dashboard" component={Dashboard} />
 								<Redirect to="/" />
 							</Switch>
--- src/components/LeaderBoard/LeaderBoard.js
+++ src/components/LeaderBoard/LeaderBoard.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect, useContext} from 'react';
+import firebase from "../../firebase"
+import { useParams } from 'react-router';
+import SmallLoader from '../Shared/SmallLoader';
+import LeaderBoardCard from "./LeaderBoardCard"
+
 
 const LeaderBoard = () => {
+    const [leaderBoardInfo, setLeaderBoardInfo] = useState([])
+    const {id} = useParams()
+
+    useEffect(() => {
+        (async () => {
+            const data = (await firebase.db.collection("Leveling").doc(id).get()).data()
+            const leaderBoardData = Object.keys(data).filter(key => data[key].xp).sort((a, b) => data[b].xp-data[a].xp)
+            const leaderBoardUsers = await Promise.all(leaderBoardData.map(async id => {
+                const response = await fetch(`${process.env.REACT_APP_API_URL}/resolveuser?user=${id}&platform=discord`)
+                return {...(await response.json()), ...data[id]}
+            }))
+            setLeaderBoardInfo(leaderBoardUsers)
+            // console.log(leaderBoardUsers)
+        })()
+    }, [id])
+
+
     return (
         <div>
-            
+            <SmallLoader loaded={leaderBoardInfo.length}/>
+            {leaderBoardInfo.map(user => <LeaderBoardCard {...user}/>)}
         </div>
     );
 }
