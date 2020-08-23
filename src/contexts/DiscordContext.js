8f710c68b5769541b1ed52a9f8e5943285956091
David Snyder <davidgraygs4@gmail.com>
2020-08-23 10:13:54 -0400

    added values to discord context

--- src/contexts/DiscordContext.js
+++ src/contexts/DiscordContext.js
@@ -1,3 +1,25 @@
-import { createContext } from "react";
+import React, { createContext } from "react";
+import { useState } from "react";
 
 export const DiscordContext = createContext({});
+
+export const DiscordContextProvider = props => {
+	const [userDiscordInfo, setUserDiscordInfo] = useState();
+    const [userConnectedGuildInfo, setUserConnectedGuildInfo] = useState();
+    const [userConnectedChannels, setUserConnectedChannels] = useState()
+
+	return (
+		<DiscordContext.Provider
+			value={{
+				userDiscordInfo,
+                setUserDiscordInfo,
+                userConnectedChannels,
+				userConnectedGuildInfo,
+                setUserConnectedChannels,
+				setUserConnectedGuildInfo,
+			}}
+		>
+			{props.children}
+		</DiscordContext.Provider>
+	);
+};
