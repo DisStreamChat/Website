baf39bb28c61ad775f9f47850fa4ea878210ac2c
David Snyder <davidgraygs4@gmail.com>
2020-08-29 11:57:02 -0400

    add modal to plugin cards

--- src/components/DashBoard/Discord/Plugins/PluginCard.js
+++ src/components/DashBoard/Discord/Plugins/PluginCard.js
@@ -1,17 +1,42 @@
-import React from "react";
-import "../PluginCard.scss"
+import React, { useState } from "react";
+import "./PluginCard.scss";
+import Modal from "react-modal";
+import { useCallback } from "react";
+
+Modal.setAppElement("#root");
 
 const PluginCard = props => {
+    const [modalOpen, setModalOpen] = useState(false);
+    
+    const handleClick = useCallback(() => {
+        if(!props.active){
+            setModalOpen(true)
+        }
+    }, [props])
+
 	return (
-		<div className={`plugin-card ${props.active ? "active" : "disabled"} ${props.comingSoon ? "coming-soon" : ""}`}>
-			<div className="image">
-				<img src={props.image} alt="" />
+		<>
+			<Modal
+				isOpen={modalOpen}
+				className="plugin-modal Modal"
+				overlayClassName="plugin-overlay Modal-Overlay"
+				onRequestClose={() => setModalOpen(false)}
+			>
+                <div className="top-portion">
+                    <h2>Enable Plugin: {props.title}</h2>
+                </div>
+                <div className="bottom-buttons"></div>
+            </Modal>
+			<div onClick={handleClick} className={`plugin-card ${props.active ? "active" : "disabled"} ${props.comingSoon ? "coming-soon" : ""}`}>
+				<div className="image">
+					<img src={props.image} alt="" />
+				</div>
+				<div className="text">
+					<h2>{props.title}</h2>
+					<h4>{props.description}</h4>
+				</div>
 			</div>
-			<div className="text">
-				<h2>{props.title}</h2>
-				<h4>{props.description}</h4>
-			</div>
-		</div>
+		</>
 	);
 };
 
