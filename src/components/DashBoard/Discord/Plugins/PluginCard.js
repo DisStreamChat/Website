a8c6f1ae76a932b398ccefb6816f31fb2b9686d1
David Snyder <davidgraygs4@gmail.com>
2020-08-29 12:18:51 -0400

    finished the look of the modal, just need to add functionality to enabling

--- src/components/DashBoard/Discord/Plugins/PluginCard.js
+++ src/components/DashBoard/Discord/Plugins/PluginCard.js
@@ -2,17 +2,24 @@ import React, { useState } from "react";
 import "./PluginCard.scss";
 import Modal from "react-modal";
 import { useCallback } from "react";
+import ClearTwoToneIcon from "@material-ui/icons/ClearTwoTone";
+import { withRouter } from "react-router";
+import A from "../../../Shared/A";
 
 Modal.setAppElement("#root");
 
 const PluginCard = props => {
-    const [modalOpen, setModalOpen] = useState(false);
-    
-    const handleClick = useCallback(() => {
-        if(!props.active){
-            setModalOpen(true)
-        }
-    }, [props])
+	const [modalOpen, setModalOpen] = useState(false);
+
+	const handleClick = useCallback(() => {
+		if (!props.active) {
+			setModalOpen(true);
+		}
+	}, [props]);
+
+	const enable = useCallback(() => {
+		props.history.push(`${props.match.url}/${props.id}`);
+	}, [props]);
 
 	return (
 		<>
@@ -22,22 +29,31 @@ const PluginCard = props => {
 				overlayClassName="plugin-overlay Modal-Overlay"
 				onRequestClose={() => setModalOpen(false)}
 			>
-                <div className="top-portion">
-                    <h2>Enable Plugin: {props.title}</h2>
-                </div>
-                <div className="bottom-buttons"></div>
-            </Modal>
-			<div onClick={handleClick} className={`plugin-card ${props.active ? "active" : "disabled"} ${props.comingSoon ? "coming-soon" : ""}`}>
-				<div className="image">
-					<img src={props.image} alt="" />
+				<div className="top-portion">
+					<h2>Enable Plugin: {props.title}</h2>
+					<button onClick={() => setModalOpen(false)}>
+						<ClearTwoToneIcon />
+					</button>
 				</div>
-				<div className="text">
-					<h2>{props.title}</h2>
-					<h4>{props.description}</h4>
+				<div className="bottom-buttons">
+					<button onClick={enable} className="enable-plugin">
+						Enable
+					</button>
 				</div>
-			</div>
+			</Modal>
+			<A href={props.active ? `${props.match.url}/leveling` : null} local>
+				<div onClick={handleClick} className={`plugin-card ${props.active ? "active" : "disabled"} ${props.comingSoon ? "coming-soon" : ""}`}>
+					<div className="image">
+						<img src={props.image} alt="" />
+					</div>
+					<div className="text">
+						<h2>{props.title}</h2>
+						<h4>{props.description}</h4>
+					</div>
+				</div>
+			</A>
 		</>
 	);
 };
 
-export default PluginCard;
+export default withRouter(PluginCard);
--- src/components/DashBoard/Discord/Plugins/PluginHome.js
+++ src/components/DashBoard/Discord/Plugins/PluginHome.js
@@ -1,18 +1,17 @@
 import React, { useEffect, useState, useCallback, useContext } from "react";
-import { Route, Redirect, Switch} from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 import firebase from "../../../../firebase";
 import A from "../../../Shared/A";
 import PluginCard from "./PluginCard";
 import { DiscordContext } from "../../../../contexts/DiscordContext";
-import Leveling from "./Leveling"
+import Leveling from "./Leveling";
 
-const PluginHome = ({match}) => {
+const PluginHome = ({ match }) => {
+	const [prefix, setPrefix] = useState("!");
+	const [activePlugins, setActivePlugins] = useState({});
+	const { userConnectedGuildInfo } = useContext(DiscordContext);
 
-    const [prefix, setPrefix] = useState("!");
-    const [activePlugins, setActivePlugins] = useState({});
-    const {userConnectedGuildInfo,} = useContext(DiscordContext)
-    
-    useEffect(() => {
+	useEffect(() => {
 		(async () => {
 			const guild = await firebase.db
 				.collection("DiscordSettings")
@@ -26,9 +25,9 @@ const PluginHome = ({match}) => {
 				setPrefix("!");
 			}
 		})();
-    }, [userConnectedGuildInfo]);
-    
-    const prefixChange = useCallback(
+	}, [userConnectedGuildInfo]);
+
+	const prefixChange = useCallback(
 		async e => {
 			setPrefix(e.target.value);
 			firebase.db
@@ -44,57 +43,69 @@ const PluginHome = ({match}) => {
 	return (
 		<>
 			<hr />
-				<div className="discord-prefix">
-					<label htmlFor="discord-prefix">
-						<h2 className="prefix-header">Command Prefix</h2>
-						<h3 className="prefix-subheader">Set the prefix used to run DisStreamBot commands in this discord server</h3>
-					</label>
-					<div className="prefix-body">
-						<input value={prefix} onChange={prefixChange} type="text" className="prefix-input" id="discord-prefix" />
-					</div>
+			<div className="discord-prefix">
+				<label htmlFor="discord-prefix">
+					<h2 className="prefix-header">Command Prefix</h2>
+					<h3 className="prefix-subheader">Set the prefix used to run DisStreamBot commands in this discord server</h3>
+				</label>
+				<div className="prefix-body">
+					<input value={prefix} onChange={prefixChange} type="text" className="prefix-input" id="discord-prefix" />
 				</div>
+			</div>
 			<hr />
-				<div className="plugins">
-					<Switch>
-						<Route exact path={`${match.url}`}>
-							<div className="plugin-header">
-								<h2>Plugins</h2>
-								<h3>Add extra functionality to the bot in your server with plugins like leveling, custom commands, and logging</h3>
-							</div>
+			<div className="plugins">
+				<Switch>
+					<Route exact path={`${match.url}`}>
+						<div className="plugin-header">
+							<h2>Plugins</h2>
+							<h3>Add extra functionality to the bot in your server with plugins like leveling, custom commands, and logging</h3>
+						</div>
 
-							<div className="plugin-list">
-								<A href={activePlugins["leveling"] ? `${match.url}/leveling` : null} local>
-									<PluginCard
-										active={activePlugins["leveling"]}
-										title="Leveling"
-										image={`${process.env.PUBLIC_URL}/trophy.svg`}
-										description="Let your users gain XP and levels by participating in the chat!"
-									/>
-								</A>
+						<div className="plugin-list">
 								<PluginCard
-									title="Custom Commands"
-									image={`${process.env.PUBLIC_URL}/discord.png`}
-									description="Add awesome custom commands to your server"
-									comingSoon
+									id="leveling"
+									active={activePlugins["leveling"]}
+									title="Leveling"
+									image={`${process.env.PUBLIC_URL}/trophy.svg`}
+									description="Let your users gain XP and levels by participating in the chat!"
 								/>
-								<PluginCard
-									title="Logging"
-									image={`${process.env.PUBLIC_URL}/clipboard.svg`}
-									description="Don't miss anything happening in your server when you are not around!"
-									comingSoon
-								/>
-								<PluginCard title="Welcome" image={`${process.env.PUBLIC_URL}/wave.svg`} description="Give new users a warm welcome" comingSoon />
-								<PluginCard title="Help" image={`${process.env.PUBLIC_URL}/help.svg`} description="Enables the 'help' command in your server" comingSoon />
-							</div>
-						</Route>
-						{activePlugins["leveling"] && (
-							<Route path={`${match.url}/leveling`}>
-								<Leveling/>
-							</Route>
-						)}
-						<Redirect to={`${match.url}`} />
-					</Switch>
-				</div>
+							<PluginCard
+								id="commands"
+								title="Custom Commands"
+								image={`${process.env.PUBLIC_URL}/discord.png`}
+								description="Add awesome custom commands to your server"
+								comingSoon
+							/>
+							<PluginCard
+								id="logging"
+								title="Logging"
+								image={`${process.env.PUBLIC_URL}/clipboard.svg`}
+								description="Don't miss anything happening in your server when you are not around!"
+								comingSoon
+							/>
+							<PluginCard
+								id="welcome"
+								title="Welcome"
+								image={`${process.env.PUBLIC_URL}/wave.svg`}
+								description="Give new users a warm welcome"
+								comingSoon
+							/>
+							<PluginCard
+								id="help"
+								title="Help"
+								image={`${process.env.PUBLIC_URL}/help.svg`}
+								description="Enables the 'help' command in your server"
+								comingSoon
+							/>
+						</div>
+					</Route>
+					<Route path={`${match.url}/leveling`}>
+						<Leveling />
+					</Route>
+
+					<Redirect to={`${match.url}`} />
+				</Switch>
+			</div>
 		</>
 	);
 };
