1ba2ce1224917ff5200dfaee7fa928c33a5b50c0
David Snyder <45735850+GypsyDangerous@users.noreply.github.com>
2020-10-07 19:37:45 -0400

    Logging module (#18)
    * minor change to bot page
    
    * added all the files
    
    * working on account settings page
    
    * made account component modular
    
    * added file for logging dashboard and added route
    
    * updated plugin descriptions
    
    * update dashbaord width
    
    * remove leveling stuff from logging file
    
    * import logging file
    
    * removed leveling code from logging useEffect
    
    * formatting
    
    * add section for logging channel
    
    * added option selector for logging channel
    
    * added events to the logging dashboard, they aren't synced with firebase yet
    
    * change width of event checkboxes
    
    * added moderation plugin
    
    * starting on custom command dashboard
    
    * added in base for channel overrides
    
    * added button to clear channel override
    
    * connected logging dashbaord to firebase
    
    * remove unused import
    
    * split role creation modals into seperate files
    
    * worked on custom commands module
    
    * added toggle to role commands that deletes after usage
    
    * work on custom commands
    
    * moved costum commands to seperate folder and display commands
    
    * added command delete function and worked on edit function
    
    * finished create command functionality and added edit functionality
    
    * add role display to role commands
    
    * add sort to log options
    
    * fixed bug with custom command creation
    
    * worked on displaying available variables
    
    * add icon to custom commands

---
+++ src/components/DashBoard/Discord/Plugins/CustomCommands/CommandItem.js
@@ -0,0 +1,104 @@
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import "./CommandItem.scss";
+import firebase from "../../../../../firebase";
+import CancelTwoToneIcon from "@material-ui/icons/CancelTwoTone";
+import { DiscordContext } from "../../../../../contexts/DiscordContext";
+import { CommandContext } from "../../../../../contexts/CommandContext";
+import RoleItem from "../../../../Shared/RoleItem";
+
+const CommandItem = ({
+	name,
+	message,
+	type,
+	description,
+	setCommands,
+	bannedRoles,
+	allowedRoles,
+	allowedChannels,
+	cooldown,
+	deleteUsage,
+	setCreatingCommand,
+	role,
+}) => {
+	const { userConnectedGuildInfo } = useContext(DiscordContext);
+	const {
+		setName,
+		setResponse,
+		setRoleToGive,
+		setDescription,
+		setAllowedRoles,
+		setBannedRoles,
+		setAllowedChannels,
+		setCooldown,
+		setDeleteUsage,
+		setError,
+		setEditing,
+	} = useContext(CommandContext);
+	const guildId = userConnectedGuildInfo.id;
+	const deleteMe = useCallback(async () => {
+		setCommands(prev => {
+			const copy = { ...prev };
+			delete copy[name];
+			return copy;
+		});
+		const commandRef = await firebase.db.collection("customCommands").doc(guildId);
+		commandRef.update({
+			[name]: firebase.app.firestore.FieldValue.delete(),
+		});
+	}, [guildId, name, setCommands]);
+
+	const edit = async () => {
+		setName(name);
+		setResponse(message);
+		if (type === "role") {
+			const roleToGive = userConnectedGuildInfo.roles.find(r => r.id === role);
+			setRoleToGive({
+				value: `${roleToGive.name}=${JSON.stringify(roleToGive)}`,
+				label: <RoleItem {...roleToGive}>{roleToGive.name}</RoleItem>,
+			});
+		}
+		setDescription(description);
+		setAllowedRoles(allowedRoles || []);
+		setAllowedChannels(allowedChannels || []);
+		setBannedRoles(bannedRoles || []);
+		setCooldown(cooldown || 0);
+		setDeleteUsage(deleteUsage);
+		setError({});
+		setEditing(true);
+		setCreatingCommand(type || "text");
+	};
+
+	const [displayRole, setDisplayRole] = useState();
+	useEffect(() => {
+		setDisplayRole(userConnectedGuildInfo.roles.find(r => r.id === role));
+	}, []);
+
+	console.log(displayRole);
+
+	return (
+		<div className="command-item">
+			<div className="delete-button" onClick={deleteMe}>
+				<CancelTwoToneIcon />
+			</div>
+			<span style={{display: "flex"}}>
+				<div className="display-image">
+					<img width="50px" src={type == "role" ? "/role.svg" : "/speech.svg"} />
+				</div>
+				<div className="command-item--info">
+					<h3>{name}</h3>
+					<h4>{description}</h4>
+				</div>
+			</span>
+			<div className="command-item--options">
+				{type === "role" && displayRole && (
+					<div className="command-role">
+						<RoleItem {...displayRole}>{displayRole.name}</RoleItem>
+					</div>
+				)}
+				<button onClick={edit}>Edit</button>
+			</div>
+		</div>
+	);
+};
+
+export default CommandItem;
--- src/components/DashBoard/Discord/Plugins/PluginHome.js
+++ src/components/DashBoard/Discord/Plugins/PluginHome.js
@@ -5,7 +5,11 @@ import A from "../../../Shared/A";
 import PluginCard from "./PluginCard";
 import { DiscordContext } from "../../../../contexts/DiscordContext";
 import Leveling from "./Leveling";
-import plugins from "./plugins.json"
+import Logging from "./Logging";
+import plugins from "./plugins.json";
+import CustomCommands from "./CustomCommands/CustomCommands";
+import { CommandContextProvider } from "../../../../contexts/CommandContext";
+
 
 const PluginHome = ({ match }) => {
 	const [prefix, setPrefix] = useState("!");
@@ -52,7 +56,7 @@ const PluginHome = ({ match }) => {
 		[userConnectedGuildInfo?.id]
 	);
 
-    const displayPlugins = useMemo(() => plugins.sort((a, b) => activePlugins[a.id] ? -1 : 1), [plugins, activePlugins])
+	const displayPlugins = useMemo(() => plugins.sort((a, b) => (activePlugins[a.id] ? -1 : 1)), [plugins, activePlugins]);
 
 	return (
 		<>
@@ -76,16 +80,28 @@ const PluginHome = ({ match }) => {
 						</div>
 
 						<div className="plugin-list">
-                            {displayPlugins.map(plugin => (
-                                <PluginCard {...plugin} active={activePlugins[plugin.id]}/>
-                            ))}
+							{displayPlugins.map(plugin => (
+								<PluginCard {...plugin} active={activePlugins[plugin.id]} />
+							))}
 						</div>
 					</Route>
-					{activePlugins["leveling"] &&
+					{activePlugins["leveling"] && (
 						<Route path={`${match.url}/leveling`}>
 							<Leveling />
 						</Route>
-					}
+					)}
+					{activePlugins["logging"] && (
+						<Route path={`${match.url}/logging`}>
+							<Logging />
+						</Route>
+					)}
+					{activePlugins["commands"] && (
+						<Route path={`${match.url}/commands`}>
+							<CommandContextProvider>
+								<CustomCommands />
+							</CommandContextProvider>
+						</Route>
+					)}
 					<Redirect to={`${match.url}`} />
 				</Switch>
 			</div>
