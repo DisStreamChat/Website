ee126ab2b2a694ead721f3130ade9e64c95a5e31
David Snyder <45735850+GypsyDangerous@users.noreply.github.com>
2020-10-10 11:47:12 -0400

    fixed issue with channels in app plugin (#22)

--- src/components/DashBoard/Discord/Plugins/PluginHome.js
+++ src/components/DashBoard/Discord/Plugins/PluginHome.js
@@ -11,23 +11,9 @@ import CustomCommands from "./CustomCommands/CustomCommands";
 import { CommandContextProvider } from "../../../../contexts/CommandContext";
 import App from "./App";
 
-const PluginHome = ({ match, guildId }) => {
+const PluginHome = ({ match, guildId, connectedGuild }) => {
 	const [prefix, setPrefix] = useState("!");
 	const { userDiscordInfo, activePlugins, setActivePlugins } = useContext(DiscordContext);
-	const [connectedGuild, setConnectedGuild] = useState();
-
-	useEffect(() => {
-		(async () => {
-			const guild = userDiscordInfo?.guilds?.find?.(guild => guild.id === guildId);
-			if (guild) {
-				const response = await fetch(`${process.env.REACT_APP_API_URL}/getchannels?new=true&guild=` + guildId);
-				const json = await response.json();
-                const roles = json.roles;
-                const channels = json.channels
-				setConnectedGuild({ ...guild, roles, channels });
-			}
-		})();
-	}, [guildId, userDiscordInfo?.guilds]);
 
 	useEffect(() => {
 		(async () => {
