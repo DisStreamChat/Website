ed16d4450cf47fe0446bfba9587fc7c48347a42b
David Snyder <davidgraygs4@gmail.com>
2020-10-12 19:34:27 -0400

    adding useReducer to role context

--- src/contexts/RoleContext.js
+++ src/contexts/RoleContext.js
@@ -1,23 +1,68 @@
-import React, { createContext } from "react";
-import { useState } from "react";
-
+import React, { createContext, useReducer } from "react";
+import lodash from "lodash";
+// lodash.get(object, nestedPropertyString);
+// lodash.set(object, path, value)
 export const RoleContext = createContext({});
 
+const Actions = {
+	EDIT: "edit",
+	CREATE: "create",
+	UPDATE: "update",
+	ERROR: "error",
+	SETUP: "setup",
+};
+
+const initialState = {
+	editing: false,
+	error: null,
+	manager: {},
+	type: "",
+};
+
+const RoleReducer = (state, action) => {
+	switch (action.type) {
+		case Actions.SETUP:
+			return initialState;
+		case Actions.EDIT:
+			return { ...state, manager: action.manager, type: action.creatingType, editing: true };
+		case Actions.CREATE:
+			return { ...state, type: action.creatingType };
+		case Actions.UPDATE:
+			const newState = { ...state };
+			lodash.set(newState, action.path, action.value);
+			return newState;
+		default:
+			return state;
+	}
+};
+
 export const RoleContextProvider = props => {
-	const [addingAction, setAddingAction] = useState(false)
-	const [editing, setEditing] = useState(false);
-    const [error, setError] = useState({});
-    
+	const [state, dispatch] = useReducer(RoleReducer, initialState);
+
+	const setup = () => dispatch({ type: Actions.SETUP });
+
+	const create = type => {
+		setup();
+		dispatch({ type: Actions.CREATE, creatingType: type });
+	};
+
+	const edit = (manager, type) => {
+		dispatch({ action: Actions.EDIT, manager, creatingType: type });
+	};
+
+	const update = (path, value) => {
+		dispatch({ action: Actions.UPDATE, path, value });
+	};
 
 	return (
 		<RoleContext.Provider
 			value={{
-				editing,
-				setEditing,
-                addingAction,
-                setAddingAction,
-				error,
-				setError,
+				state,
+				dispatch,
+				setup,
+				create,
+				edit,
+				update,
 			}}
 		>
 			{props.children}
