802411402c1746018adb43e64ef6257612c69dc8
David Snyder <davidgraygs4@gmail.com>
2020-10-15 08:30:34 -0400

    load account data in account setting page

--- src/components/DashBoard/Account/Account.js
+++ src/components/DashBoard/Account/Account.js
@@ -1,15 +1,40 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AppContext } from "../../../contexts/Appcontext";
 import "./Accounts.scss";
 import AccountComponent from "./AccountComponent";
+import firebase from "../../../firebase";
 
 const Account = () => {
-	const { currentUser } = useContext(AppContext);
+	const currentUser = firebase.auth.currentUser;
+	const [discordAccount, setDiscordAccount] = useState();
+	const [twitchAccount, setTwitchAccount] = useState();
+
+	useEffect(() => {
+		(async () => {
+			const userRef = firebase.db.collection("Streamers").doc(currentUser?.uid);
+			const discordRef = userRef.collection("discord").doc("data");
+			const twitchRef = userRef.collection("twitch").doc("data");
+			const userData = (await userRef.get()).data();
+			if (!userData) return;
+			const discordData = (await discordRef.get()).data();
+			const twitchData = (await twitchRef.get()).data();
+			if (discordData) {
+				const {name, profilePicture} = discordData
+				setDiscordAccount({name, profilePicture});
+			} else {
+				setDiscordAccount(null);
+			}
+			if(twitchData){
+				const {name, profilePicture} = userData
+				setTwitchAccount({name, profilePicture});
+			}
+		})();
+	}, [currentUser]);
 
 	return (
 		<div classname="accounts" style={{ width: "100%" }}>
-			<AccountComponent platform="twitch" />
-			<AccountComponent platform="discord" />
+			{twitchAccount && <AccountComponent {...twitchAccount} platform="twitch" />}
+			{discordAccount && <AccountComponent {...discordAccount} platform="discord" />}
 		</div>
 	);
 };
--- src/components/DashBoard/Account/AccountComponent.js
+++ src/components/DashBoard/Account/AccountComponent.js
@@ -1,18 +1,25 @@
 import React from "react";
+import styled from "styled-components"
 
 const logos = {
     "discord": "https://cdn.iconscout.com/icon/free/png-512/discord-3-569463.png",
     "twitch": "/social-media.svg"
 }
 
-const DiscordComponent = ({platform}) => {
+const AccountName = styled.p`
+	text-transform: capitalize;
+`
+
+
+const DiscordComponent = ({profilePicture, platform, name}) => {
 	return (
 		<div className={`account discord`}>
 			<div className={`account-header ${platform}`}>
 				<img src={logos[platform]} alt="" />
+				<img src={profilePicture} alt=""/>
                 <div className="name">
-                    <p></p>
-                    <p>Account Name</p>
+                    <AccountName>{name}</AccountName>
+					<p>Account Name</p>
                 </div>
 			</div>
 			<div className={`account-body ${platform}`}></div>
