8dd029d2f1ef976977e36e5328c9d08943a98e24
David Snyder <45735850+GypsyDangerous@users.noreply.github.com>
2020-10-15 09:06:12 -0400

    Revert "Account settings (#28)"
    This reverts commit ee6ad10b8100585b75013e4880fd7c914f84fb05.

--- src/components/DashBoard/Account/AccountComponent.js
+++ src/components/DashBoard/Account/AccountComponent.js
@@ -1,54 +1,21 @@
 import React from "react";
-import styled from "styled-components";
-import firebase from "../../../firebase";
 
 const logos = {
-	discord: "https://cdn.iconscout.com/icon/free/png-512/discord-3-569463.png",
-	twitch: "/social-media.svg",
-};
+    "discord": "https://cdn.iconscout.com/icon/free/png-512/discord-3-569463.png",
+    "twitch": "/social-media.svg"
+}
 
-const AccountName = styled.p`
-	text-transform: capitalize;
-`;
-
-const DiscordComponent = ({ main, profilePicture, platform, name }) => {
-	const currentUser = firebase.auth.currentUser;
-	const disconnect = async () => {
-		await firebase.db
-			.collection("Streamers")
-			.doc(currentUser?.uid || " ")
-			.collection(platform)
-			.doc("data")
-			.delete();
-	};
-
-	const deleteAccount = async () => {
-		await firebase.db
-			.collection("Streamers")
-			.doc(currentUser?.uid || " ")
-			.delete();
-		try {
-			await currentUser.delete();
-		} catch (err) {
-			// re-sign in and delete
-		}
-	};
-
+const DiscordComponent = ({platform}) => {
 	return (
 		<div className={`account discord`}>
 			<div className={`account-header ${platform}`}>
 				<img src={logos[platform]} alt="" />
-				<img src={profilePicture} alt="" />
-				<div className="name">
-					<AccountName>{name}</AccountName>
-					<p>Account Name</p>
-				</div>
+                <div className="name">
+                    <p></p>
+                    <p>Account Name</p>
+                </div>
 			</div>
-			<div className={`account-body ${platform}`}>
-				{main !== platform && (
-					<button onClick={() => (main === platform ? null : disconnect())}>{main === platform ? "Delete" : "Disconnect"} Account</button>
-				)}
-			</div>
+			<div className={`account-body ${platform}`}></div>
 		</div>
 	);
 };
