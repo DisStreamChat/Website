ee6ad10b8100585b75013e4880fd7c914f84fb05
David Snyder <45735850+GypsyDangerous@users.noreply.github.com>
2020-10-15 09:06:08 -0400

    Account settings (#28)
    * load account data in account setting page
    
    * added disconnect account button

--- src/components/DashBoard/Account/Account.js
+++ src/components/DashBoard/Account/Account.js
@@ -1,15 +1,46 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AppContext } from "../../../contexts/Appcontext";
 import "./Accounts.scss";
 import AccountComponent from "./AccountComponent";
+import firebase from "../../../firebase";
 
 const Account = () => {
-	const { currentUser } = useContext(AppContext);
+	const currentUser = firebase.auth.currentUser;
+	const [discordAccount, setDiscordAccount] = useState();
+	const [twitchAccount, setTwitchAccount] = useState();
+	const [mainAccount, setMainAccount] = useState();
+
+	useEffect(() => {
+		(async () => {
+			const userRef = firebase.db.collection("Streamers").doc(currentUser?.uid);
+			const discordRef = userRef.collection("discord").doc("data");
+			const twitchRef = userRef.collection("twitch").doc("data");
+			const userData = (await userRef.get()).data();
+
+			if (!userData) return;
+			const twitchData = (await twitchRef.get()).data();
+			discordRef.onSnapshot(snapshot => {
+				const discordData = snapshot.data()
+				if (discordData) {
+					const { name, profilePicture } = discordData;
+					setDiscordAccount({ name, profilePicture });
+				} else {
+					setDiscordAccount(null);
+				}
+			});
+			if (twitchData) {
+				const { name, profilePicture } = userData;
+				setTwitchAccount({ name, profilePicture });
+			}
+			if(userData.discordLinked) setMainAccount("discord")
+			else if(userData.twitchAuthenticated) setMainAccount("twitch")
+		})();
+	}, [currentUser]);
 
 	return (
 		<div classname="accounts" style={{ width: "100%" }}>
-			<AccountComponent platform="twitch" />
-			<AccountComponent platform="discord" />
+			{twitchAccount && <AccountComponent main={mainAccount} {...twitchAccount} platform="twitch" />}
+			{discordAccount && <AccountComponent main={mainAccount} {...discordAccount} platform="discord" />}
 		</div>
 	);
 };
