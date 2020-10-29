9dec34368d4cd42edc94c837d578ea4e43191f05
David Snyder <davidgraygs4@gmail.com>
2020-10-29 19:40:48 -0400

    began moving utitily functions and constants to seperate files

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,3 +1,5 @@
+import GuildIcon from "../styled-components/GuildIcon"
+
 export function getOS() {
 	var userAgent = window.navigator.userAgent,
 		platform = window.navigator.platform,
@@ -22,3 +24,34 @@ export function getOS() {
 }
 
 export const ArrayAny = (arr1, arr2) => arr1.some(v => arr2.indexOf(v) >= 0);
+
+export const TransformObjectToSelectValue = (obj, key="id") => {
+	return `${obj[key]}=${JSON.stringify(obj)}`
+}
+
+export const guildOption = guild => {
+	if (!guild) return null;
+	const size = 40;
+	return {
+		value: guild.name,
+		label: (
+			<span style={{ height: size, display: "flex", alignItems: "center" }}>
+				<GuildIcon size={size} {...guild} />
+				{guild.name}
+			</span>
+		),
+	};
+};
+
+export const parseSelectValue = value => {
+	if (value instanceof Array) {
+		if (value.length === 0) return value;
+		return value.map(role => JSON.parse(role.value.split("=")[1])).map(val => val.id);
+	} else {
+		try {
+			return JSON.parse(value.value.split("=")[1]).id;
+		} catch (err) {
+			return null;
+		}
+	}
+};
