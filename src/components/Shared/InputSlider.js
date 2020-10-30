d564e254f86608ca1f0d15f93b4a2600095921ac
David Snyder <davidgraygs4@gmail.com>
2020-10-29 21:12:24 -0400

    moved more styled components to seperate files

--- src/components/Shared/InputSlider.js
+++ src/components/Shared/InputSlider.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { makeStyles, withStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import Slider from "@material-ui/core/Slider";
 import Input from "@material-ui/core/Input";
+import PrettoSlider from "../../styled-components/PrettoSlider"
 
 const useStyles = makeStyles({
 	root: {
@@ -11,65 +11,30 @@ const useStyles = makeStyles({
 		marginLeft: 8,
 	},
 	input: {
-        color: "white",
-        borderBottom: "1px solid white",
-        marginBottom: 8,
-    },
+		color: "white",
+		borderBottom: "1px solid white",
+		marginBottom: 8,
+	},
 });
 
-const PrettoSlider = withStyles({
-	root: {
-		color: "#fff",
-        height: 8,
-        marginBottom: 16,
-        paddingTop: 4,
-        boxSizing: "content-box"
-	},
-	thumb: {
-		height: 24,
-		width: 24,
-		backgroundColor: "#fff",
-		border: "4px solid #2D688D",
-		marginTop: 0,
-		"&:focus, &:hover, &:active": {
-			boxShadow: "inherit",
-		},
-	},
-	active: {},
-	valueLabel: {
-        color: "black",
-		left: "calc(-50% - 4px)",
-	},
-	track: {
-		height: 24,
-        borderRadius: 24,
-        paddingRight: 12,
-        boxSizing: "content-box !important"
-	},
-	rail: {
-        overflow: "hidden",
-		height: 24,
-		borderRadius: 24,
-	},
-})(Slider);
 
 const InputSlider = props => {
 	const classes = useStyles();
 	const [value, setValue] = useState(props.defaultValue);
 
-	const handleSliderChange = (event, newValue) => {
-		setValue(newValue);
-	};
+	// const handleSliderChange = (event, newValue) => {
+	// 	setValue(newValue);
+	// };
 
-	const handleInputChange = event => {
-		setValue(event.target.value === "" ? "" : Number(event.target.value));
-    };
-    
-    useEffect(() => {
-        if(props.value){
-            setValue(props.value)
-        }
-    }, [props])
+	// const handleInputChange = event => {
+	// 	setValue(event.target.value === "" ? "" : Number(event.target.value));
+	// };
+
+	useEffect(() => {
+		if (props.value) {
+			setValue(props.value);
+		}
+	}, [props]);
 
 	const handleBlur = () => {
 		if (value < props.min) {
@@ -92,9 +57,8 @@ const InputSlider = props => {
 						onChange={props.onSliderChange}
 						aria-labelledby="input-slider"
 						min={props.min}
-                        max={props.max}
-                        step={props.step||1}
-                        
+						max={props.max}
+						step={props.step || 1}
 					/>
 				</Grid>
 				<Grid item>
@@ -116,6 +80,6 @@ const InputSlider = props => {
 			</Grid>
 		</div>
 	);
-}
+};
 
-export default InputSlider
+export default InputSlider;
