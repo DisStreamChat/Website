2542fc1afee1d5b99d3fb24405588b778639b77a
David Snyder <davidgraygs4@gmail.com>
2020-10-30 19:02:46 -0400

    moved settingslist to seperate file

--- src/components/DashBoard/Settings/SettingBox.js
+++ src/components/DashBoard/Settings/SettingBox.js
@@ -1,61 +1,12 @@
 import { useState, useEffect, useCallback } from "react";
-import { Switch, NavLink, Route, Redirect, useParams } from "react-router-dom";
+import { Switch, NavLink, Route, Redirect } from "react-router-dom";
 import SettingAccordion from "./SettingAccordion";
 import Setting from "./Setting";
 import ClearRoundedIcon from "@material-ui/icons/ClearRounded";
 import { useContext } from "react";
 import { DiscordContext } from "../../../contexts/DiscordContext";
-import { useMemo } from "react";
+import SettingList from "./SettingList";
 
-const SettingList = ({ search, app, all, defaultSettings, ...props }) => {
-	const { key } = useParams();
-	const [index, setIndex] = useState(key);
-
-	useEffect(() => {
-		if (props.index) {
-			setIndex(props.index);
-		} else if (key) {
-			setIndex(key);
-		}
-	}, [props, key]);
-
-	const displaySettings = useMemo(() => {
-		return Object.entries(defaultSettings || {})
-			.filter(([name]) => (!search ? true : name?.toLowerCase()?.includes(search?.toLowerCase())))
-			.filter(([, details]) => details.category?.toLowerCase() === index?.toLowerCase() || all)
-			.filter(([, details]) => (app ? true : !details.appOnly))
-			.filter(([, details]) => details.type !== "keybind")
-			.sort((a, b) => {
-				const categoryOrder = a[1].type.localeCompare(b[1].type);
-				const nameOrder = a[0].localeCompare(b[0]);
-				return !!categoryOrder ? categoryOrder : nameOrder;
-			});
-	}, [index, all, app, defaultSettings, search]);
-
-	return (
-		<SettingAccordion>
-			{displaySettings.map(([key, value]) => {
-				if (!props.settings) return <></>;
-				return (
-					<Setting
-						default={value.value}
-						key={key}
-						index={index}
-						onChange={props.updateSettings}
-						value={props?.settings?.[key]}
-						name={key}
-						type={value.type}
-						min={value.min}
-						max={value.max}
-						step={value.step}
-						placeholder={value.placeholder}
-						options={value.options}
-					/>
-				);
-			})}
-		</SettingAccordion>
-	);
-};
 
 const SettingBox = props => {
 	const { userDiscordInfo } = useContext(DiscordContext);
@@ -79,25 +30,52 @@ const SettingBox = props => {
 			<h3>{props.subtitle}</h3>
 			<hr />
 			<span className="search-container">
-				<input value={search} onChange={handleChange} type="text" name="" id="" placeholder="Search" className="settings--searchbox" />
+				<input
+					value={search}
+					onChange={handleChange}
+					type="text"
+					name=""
+					id=""
+					placeholder="Search"
+					className="settings--searchbox"
+				/>
 				<ClearRoundedIcon className="clear-button" onClick={resetSearch} />
 			</span>
 
 			<span className="settings-sub-body">
 				<div className="settings-categories">
-					<NavLink activeClassName="active-category" className="category" to={`${props.parenturl}/${props.path}/all`}>
+					<NavLink
+						activeClassName="active-category"
+						className="category"
+						to={`${props.parenturl}/${props.path}/all`}
+					>
 						All
 					</NavLink>
 					{userDiscordInfo && (
-						<NavLink activeClassName="active-category" className="category" to={`${props.parenturl}/${props.path}/discord`}>
+						<NavLink
+							activeClassName="active-category"
+							className="category"
+							to={`${props.parenturl}/${props.path}/discord`}
+						>
 							Discord
 						</NavLink>
 					)}
-					{[...new Set(Object.values(props.defaultSettings || {}).map(val => val.category))]
+					{[
+						...new Set(
+							Object.values(props.defaultSettings || {}).map(val => val.category)
+						),
+					]
 						.sort()
-						.filter(cat => (cat ? (props.app ? true : cat.toLowerCase() !== "visibility") : false))
+						.filter(cat =>
+							cat ? (props.app ? true : cat.toLowerCase() !== "visibility") : false
+						)
 						.map(key => (
-							<NavLink activeClassName="active-category" className="category" to={`${props.parenturl}/${props.path}/${key}`} key={key}>
+							<NavLink
+								activeClassName="active-category"
+								className="category"
+								to={`${props.parenturl}/${props.path}/${key}`}
+								key={key}
+							>
 								{key}
 							</NavLink>
 						))}
