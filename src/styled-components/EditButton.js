d8f196612c7298ff8b5a05043770b6f242b7d05c
David Snyder <davidgraygs4@gmail.com>
2020-10-31 20:40:52 -0400

    added editbutton styled component

---
+++ src/styled-components/EditButton.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const EditButton = styled.button`
+	padding: 0.5rem 1.5rem;
+	border-radius: 0.25rem;
+	border: 1px solid rgba(170, 170, 170, 0.75);
+	background: #00000000;
+	color: white;
+	transition: 0.25s;
+	outline: none !important;
+	cursor: pointer;
+`;
+
+export default EditButton
