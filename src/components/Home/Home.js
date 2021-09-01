d4381431210be2da368b542b3900930786fa5aba
Panquesito7 <halfpacho@gmail.com>
2021-09-01 00:38:32 +0000

    fix: Minor spelling/comma improvements

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -18,7 +18,7 @@ const Home = () => {
 				<div className="header-area">
 					<h1 className="body-header">Integrate your Discord server with Twitch</h1>
 					<h3 className="body-subheader">
-						Chat, moderation, interactivity, and much more easily Integrated with Twitch
+						Chat, moderation, interactivity, and much more easily integrated with Twitch
 						and Discord!
 					</h3>
 				</div>
@@ -34,7 +34,7 @@ const Home = () => {
 					</A>
 					{currentUser && (
 						<Link to="/dashboard" className="dashboard-button">
-							My DashBoard
+							My Dashboard
 						</Link>
 					)}
 				</div>
@@ -44,13 +44,13 @@ const Home = () => {
 				<div className="landing" id="features">
 					<Feature
 						title="Manage all your chats from one app"
-						body="Open, interact with, moderate and do so much more for any channel you own or moderate for. You can even open multiple
-							chats at once by ctrl+clicking them and be a super-moderator of doom!"
+						body="Open, interact with, moderate, and do so much more for any channel you own or moderate for. You can even open multiple
+							chats at once by Ctrl+clicking them and be a super-moderator of doom!"
 						images={[`${process.env.PUBLIC_URL}/home.png`]}
 					></Feature>
 					<Feature
 						title="Customize just about anything"
-						body="You have the option to customize an abundance of different settings both within the app and on the website. Updating a
+						body="You have the option to customize an abundance of different settings, both within the app and on the website. Updating a
                 setting anywhere will apply them on the client immediately so you could change those secret settings off screen."
 						images={[
 							`${process.env.PUBLIC_URL}/merged2.png`,
@@ -60,15 +60,15 @@ const Home = () => {
 					></Feature>
 					<Feature
 						title="Never miss a new follower again"
-						body="Get all the notifications you could ever wish for in the app, like followers, subscriptions, bits and many more. Customize
+						body="Get all the notifications you could ever wish for in the app, like followers, subscriptions, bits, and many more. Customize
                 them to fit your style or ignore them completely, whichever serves you and your army of viewing minions best."
 						images={[`${process.env.PUBLIC_URL}/notifications.png`]}
 					></Feature>
 					<Feature
 						title="No more scrolling for days looking for that one message"
-						body="DisStreamChat provides an easy to use searchbar that you can access through ctrl+f, not only does it allow you to search
+						body="DisStreamChat provides an easy-to-use searchbar that you can access through Ctrl+f, not only does it allow you to search
                 for that amazing cat gif you just missed, it also allows you to use tags to search for things like follows, links,
-                usernames and plenty more."
+                usernames, and plenty more."
 						images={[`${process.env.PUBLIC_URL}/merged1.png`]}
 						imageClassNames={["drop-shadow"]}
 						reversed
@@ -82,8 +82,8 @@ const Home = () => {
 						images={[`${process.env.PUBLIC_URL}/unfocus.png`]}
 					></Feature>
 					<Feature
-						title="Integrate your super-secret exclusive discord chat channel"
-						body="Set up the discord bot to listen to any channels you want and integrate any chat going on in those chats into your chat
+						title="Integrate your super-secret exclusive Discord chat channel"
+						body="Set up the Discord bot to listen to any channels you want and integrate any chat going on in those chats into your chat
 							client. At least now you can see your moderators talking about your messed up audio right from your own chat."
 						images={[`${process.env.PUBLIC_URL}/discordmessage.png`]}
 						reversed
